49d9d95c4f28c342c7789ab0e41dbf52261fc805
anouther <155570107+anouther@users.noreply.github.com>
2025-01-27 13:00:11 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
       const c = setTimeout(() => {
         document.body.classList.remove("not-loaded");
 
-        const titles = ('CAJUL, NEOMU YEPPEO').split('');
+        const titles = ('Happy Birthday').split('');
         const titleElement = document.getElementById('title');
         let index = 0;
 
@@ -21,7 +21,7 @@
           } else {
             // Setelah teks utama selesai, tambahkan subtitle kecil
             const subtitle = document.createElement('p');
-            subtitle.innerText = "Semoga di tahun ini ka cajul bahagia dan semua wishlistnya terwujud ya Kaka cantik🤍🤍 ";
+            subtitle.innerText = "happy birthday bff ku yang paling imout! maap ya kado dari akunya telat hehe bingung soalnya mau kasih apa buat kamu. wishnya semoga kamu sehat selalu, panjang umur, lancar terus rezekinya (biar bisa traktir ak, ups ups) kamu tw gaa si? aku tu sayanggg banget sama kamu, rasa sayangnya tu sebanyak buih di lautan (anjay) pokoknya besarrrr banget deh rasa sayang aku. makasii banyak udah mw jadi temen aku ya sayangq. aku bersyukur banget punya temen sebaik kamu. semoga di ulang tahun kamu ini, kamu bisa jadi pribadi yang lebih baik ";
             subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
             subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
             subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
